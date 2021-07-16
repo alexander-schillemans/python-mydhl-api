6d773236b8fbe5bf0b3ac5d8d745a7c97649de61
Alexander Schillemans <alexander.schillemans@lhs.global>
2021-07-16 09:08:28 +0200

    Added clientdetail & request to shipment request

--- mydhl/models/shipmentrequests.py
+++ mydhl/models/shipmentrequests.py
@@ -1,9 +1,16 @@
 from .base import ObjectListModel, BaseModel
+from .general import ClientDetail, Request, ServiceHeader
 
 class ShipmentRequest(BaseModel):
 
     def __init__(self,
-        requestedshipment
+        messageid=None,
+        clientdetail=None,
+        request=None,
+        requestedshipment=None
     ):
 
+        self.messageid = messageid
+        self.clientdetail = clientdetail if clientdetail else ClientDetail()
+        self.request = request if request else Request()
         self.requestedshipment = requestedshipment if requestedshipment else RequestedShipment()
