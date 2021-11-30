05dd149568a6cd337dafaea957950ec2e70cf5ed
Alexander Schillemans <alexander.schillemans@lhs.global>
2021-11-30 16:27:26 +0100

    Added support for PaperlessTrade and ExportLine for customs

--- mydhl/endpoints/shipmentrequests.py
+++ mydhl/endpoints/shipmentrequests.py
@@ -9,25 +9,27 @@ class ShipmentRequestMethods(APIEndpoint):
     def __init__(self, api):
         super(ShipmentRequestMethods, self).__init__(api, "ShipmentRequest")
     
-
     def shipPackage(self,
         serviceType,
         shipper, 
         recipient,
         packages,
         internationalDetail,
+        labelOptions=None,
         shipTimestamp=None,
         dropOffType=DropOffType.REGULAR_PICKUP, 
         unitOfMeasurement=Measurement.MEASUREMENT_METRIC,
         paymentInfo=PaymentInfo.PAYMENT_DAP,
-        currency='EUR'
+        currency='EUR',
+        invoicePDF=None,
     ):
 
+        # Create timestamp from now +2 hours if timestamp is not given
         if not shipTimestamp: 
             shipTime = datetime.datetime.now() + datetime.timedelta(hours=2)
             shipTimestamp = shipTime.strftime('%Y-%m-%dT%H:%M:%S')
         
-
+        # Starting data
         data = {
             "ShipmentRequest": {
                 "RequestedShipment": {
@@ -50,12 +52,33 @@ class ShipmentRequestMethods(APIEndpoint):
                 }
             }
         }
+        
+        # If labelOptions is added, send it with the request
+        if labelOptions: data['ShipmentRequest']['RequestedShipment']['ShipmentInfo']['LabelOptions'] = labelOptions.getJSON()
+        
+        # If PDF of invoice is added, send it with the request
+        if invoicePDF:
+            specialServices = SpecialServices()
+            serv = SpecialService(serviceType="WY")
+            specialServices.add(serv)
+            
+            # Enable paperless trade
+            data['ShipmentRequest']['RequestedShipment']['ShipmentInfo']['PaperlessTradeEnabled'] = 'true'
+            
+            # Add WY service
+            data['ShipmentRequest']['RequestedShipment']['ShipmentInfo']['SpecialServices'] = [{ 'Service' : [] }]
+            data['ShipmentRequest']['RequestedShipment']['ShipmentInfo']['SpecialServices'][0]['Service'] = specialServices.getJSON()
+            
+            # Add image in B64
+            data['ShipmentRequest']['RequestedShipment']['ShipmentInfo']['PaperlessTradeImage'] = invoicePDF
+            data['ShipmentRequest']['RequestedShipment']['ShipmentInfo']['DocumentImages'] = [{ 'DocumentImage' : { 'DocumentImageType' : 'DCL', 'DocumentImage' : invoicePDF, 'DocumentImageFormat' : 'PDF'}}]
+
 
         print(data)
 
-        url = self.endpoint
+        # url = self.endpoint
 
-        status, headers, respJson = self.api.post(url, data)
+        # status, headers, respJson = self.api.post(url, data)
 
-        print(respJson)
-        return ShipmentResponse().parse(respJson['ShipmentResponse'])
+        # print(respJson)
+        # return ShipmentResponse().parse(respJson['ShipmentResponse'])
